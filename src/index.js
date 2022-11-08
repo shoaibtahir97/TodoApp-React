43d143b9de205c1c888104fe1d581dd0a7944d25
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2022-11-08 10:37:05 -0800

    updated README.md

--- src/index.js
+++ src/index.js
@@ -1,93 +1,93 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function TodoApp() {
   let [list, setList] = useState([]); //created a state to store the todo items
-    let [text, setText] = useState('');
-    let [todo, setTodo] = useState();
-    let [btn, setBtn] = useState(true);
+  let [text, setText] = useState('');
+  let [todo, setTodo] = useState();
+  let [btn, setBtn] = useState(true);
 
 
-    const addTodo = () => {
-        if(text !== "") {
+  const addTodo = () => {
+    if (text !== "") {
 
-            let tempList = [...list]
-            tempList.push(text);
-            setList(tempList)
-            
-            setText("")
-        }else{
-            alert("Fill the Todo")
-        }
+      let tempList = [...list]
+      tempList.push(text);
+      setList(tempList)
 
+      setText("")
+    } else {
+      alert("Fill the Todo")
     }
 
-    const getInput = (e) => {
-        setText(e.target.value)
-    }
+  }
 
-    const deleteTodo = (index) => {
-        let deletedList = [...list];
-        deletedList.splice(index, 1)
-        setList(deletedList);
-    }
+  const getInput = (e) => {
+    setText(e.target.value)
+  }
 
-    const updateTodo = (item, index) => {
-        let updateList = [...list]; //make copy of array 
-        let returedValue = updateList.splice(index, 1); //remove the item from array
-        setList(updateList) //update the list on UI after removal of item
-        setText(returedValue)//show the removed item on input text to be updated 
-        //set the updated Todo on the same position
+  const deleteTodo = (index) => {
+    let deletedList = [...list];
+    deletedList.splice(index, 1)
+    setList(deletedList);
+  }
 
-        setTodo(index)
-        setBtn(false)
+  const updateTodo = (item, index) => {
+    let updateList = [...list]; //make copy of array 
+    let returedValue = updateList.splice(index, 1); //remove the item from array
+    setList(updateList) //update the list on UI after removal of item
+    setText(returedValue)//show the removed item on input text to be updated 
+    //set the updated Todo on the same position
 
-    }
+    setTodo(index)
+    setBtn(false)
 
-    const editTodo = () => {
-        let changeList = [...list];
-        changeList.splice(todo, 0, text)
-        setList(changeList)
-        setBtn(true)
-        setText("")
-    }
+  }
 
-    return (
-        <div className='main'>
-            <h1>Todo App</h1>
+  const editTodo = () => {
+    let changeList = [...list];
+    changeList.splice(todo, 0, text)
+    setList(changeList)
+    setBtn(true)
+    setText("")
+  }
 
-            <input value={text} type="text" placeholder="Enter To Do" onChange={getInput} />
-            <br/>
-            <br />
-            {
+  return (
+    <div className='main'>
+      <h1>Todo App</h1>
 
-                btn ?
-                    <button className='addTodo' onClick={addTodo}>Add New Task</button>
-                    :
-                    <button className='editTodo' onClick={editTodo}>Edit Todo</button>
-            }
+      <input value={text} type="text" placeholder="Enter To Do" onChange={getInput} />
+      <br />
+      <br />
+      {
+
+        btn ?
+          <button className='addTodo' onClick={addTodo}>Add New Task</button>
+          :
+          <button className='editTodo' onClick={editTodo}>Edit Todo</button>
+      }
 
 
-            <div>
-                {list.map((item, index) => {
-                    return (
-                        <div className='todo'>
-                            {item}
-                            <br />
-                            <button className='delBtn' onClick={() => deleteTodo(index)}>Delete</button>
-                            <button className = "UpdateBtn" onClick={() => updateTodo(item, index)}>Update</button>
-                        </div>
-                    )
-                })}
+      <div>
+        {list.map((item, index) => {
+          return (
+            <div className='todo'>
+              {item}
+              <br />
+              <button className='delBtn' onClick={() => deleteTodo(index)}>Delete</button>
+              <button className="UpdateBtn" onClick={() => updateTodo(item, index)}>Update</button>
             </div>
+          )
+        })}
+      </div>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 ReactDOM.render(
-  <TodoApp/>,
+  <TodoApp />,
   document.querySelector("#root")
 )
 
